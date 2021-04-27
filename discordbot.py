c8636d31d88dc90c46726bc533170ce23add6f42
e1b18051 <56813109+e1b18051@users.noreply.github.com>
2021-04-27 15:32:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,19 +11,19 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@client.event
-async def on_message(message):
+# @client.event
+# async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
+#     if message.author.bot:
+#         return
     # 「入室」と発言したら「入室しました」とリプライされる処理
-    if message.content == '入室':
-        reply = f'{message.author.mention} が入室しました' # 返信メッセージの作成
-        await message.channel.send(reply) # 返信メッセージを送信
+#     if message.content == '入室':
+#         reply = f'{message.author.mention} が入室しました' # 返信メッセージの作成
+#         await message.channel.send(reply) # 返信メッセージを送信
     # 「退室」と発言したら「退室しました」とリプライされる処理
-    if message.content == '退室':
-        reply = f'{message.author.mention} が退室しました' # 返信メッセージの作成
-        await message.channel.send(reply) # 返信メッセージを送信
+#     if message.content == '退室':
+#         reply = f'{message.author.mention} が退室しました' # 返信メッセージの作成
+#         await message.channel.send(reply) # 返信メッセージを送信
 
 
 @bot.command()
